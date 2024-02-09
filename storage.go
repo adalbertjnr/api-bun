ce3584e7b7689721fc528f03d8a1521fbc45cb6a
Adalberto Junior <adalberto.gmu@gmail.com>
2024-02-09 14:48:19 -0300

    store interface

---
+++ storage.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+)
+
+type Storager interface {
+	CreateAccount(account *Account) error
+	DeleteAccount(id int) error
+	UpdateAccount(account *Account) error
+	GetAccountById(id int) (*Account, error)
+	Init() error
+}
+
+type PostgresStore struct {
+	db  *bun.DB
+	ctx context.Context
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
+	sqldb, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := sqldb.Ping(); err != nil {
+		return nil, err
+	}
+	db := bun.NewDB(sqldb, pgdialect.New())
+	return &PostgresStore{
+		db:  db,
+		ctx: context.TODO(),
+	}, nil
+}
+
+func (s *PostgresStore) Init() error {
+	if err := s.CreateAccountTable(); err != nil {
+		return fmt.Errorf("failed to create account table %w", err)
+	}
+	return nil
+}
+
+func (s *PostgresStore) CreateAccountTable() error {
+	_, err := s.db.NewCreateTable().Model((*Account)(nil)).IfNotExists().Exec(s.ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *PostgresStore) CreateAccount(account *Account) error {
+	_, err := s.db.NewInsert().
+		Model(account).
+		Exec(s.ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(account *Account) error {
+	return nil
+}
+
+func (s *PostgresStore) GetAccountById(id int) (*Account, error) {
+	return nil, nil
+}
