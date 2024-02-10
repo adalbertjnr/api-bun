b005d75ef951da1bfd6cb3dca5ecc8a390e9d7b1
Adalberto Junior <adalberto.gmu@gmail.com>
2024-02-10 20:56:34 -0300

    implementing jwt

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,7 +1,9 @@
-package jwt
+package midd
 
 import (
 	"api/internal/util"
+	"api/store"
+	"api/types"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,12 +11,27 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func JWTAuthentication(handlerFnWithJWT http.HandlerFunc) http.HandlerFunc {
+func JWTAuthentication(handlerFnWithJWT http.HandlerFunc, s store.Storager) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("x-hunter999-token")
-		_, err := jwtValidator(tokenString)
+		token, err := jwtValidator(tokenString)
+		if err != nil || !token.Valid {
+			util.WriteJSONResponse(w, http.StatusForbidden, util.NewError(types.ErrAccessDenied))
+			return
+		}
+		userId, err := util.IDGetter(r)
 		if err != nil {
-			util.WriteJSONResponse(w, http.StatusForbidden, map[string]string{"error": "invalid token"})
+			util.WriteJSONResponse(w, http.StatusForbidden, util.NewError(types.ErrAccessDenied))
+			return
+		}
+		account, err := s.GetAccountById(userId)
+		if err != nil {
+			util.WriteJSONResponse(w, http.StatusForbidden, util.NewError(types.ErrAccessDenied))
+			return
+		}
+		claims := token.Claims.(jwt.MapClaims)
+		if account.Number != claims["accountNumber"] {
+			util.WriteJSONResponse(w, http.StatusForbidden, util.NewError(types.ErrAccessDenied))
 			return
 		}
 		handlerFnWithJWT(w, r)
@@ -28,4 +45,14 @@ func jwtValidator(tokenString string) (*jwt.Token, error) {
 		}
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
+}
+
+func NewJWTToken(account *types.Account) (string, error) {
+	claims := jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
