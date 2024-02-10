3fe19355bc1eb745661a893033497748dd206998
Adalberto Junior <adalberto.gmu@gmail.com>
2024-02-10 20:56:06 -0300

    r id getter

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,10 +3,25 @@ package util
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+func NewError(newError error) map[string]string {
+	return map[string]string{"err": newError.Error()}
+}
+
+func IDGetter(r *http.Request) (int, error) {
+	IdVars, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		return 0, err
+	}
+	return IdVars, nil
+}
+
 func WriteJSONResponse(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Header().Add("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
