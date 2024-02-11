4c080e66baba666c6fb0d8e346ce7c602a810d7e
Adalberto Junior <adalberto.gmu@gmail.com>
2024-02-11 17:58:19 -0300

    bcrypt and login params

--- types/types.go
+++ types/types.go
@@ -6,11 +6,18 @@ import (
 	"time"
 
 	"github.com/uptrace/bun"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
-	ErrAccessDenied = errors.New("access denied")
+	ErrAccessDenied     = errors.New("access denied")
+	ErrMethodNotAllowed = errors.New("method not allowed")
 )
+
+type LoginParams struct {
+	Number   int    `json:"number"`
+	Password string `json:"password"`
+}
 
 type TransferRequest struct {
 	DestAccount int `json:"dest_account"`
@@ -20,23 +27,30 @@ type TransferRequest struct {
 type CreateAccoutRequest struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+	Password  string `json:"password"`
 }
 
 type Account struct {
-	bun.BaseModel `bun:"table:accounts,alias:a"`
-	ID            int       `bun:",pk,autoincrement" json:"id"`
-	FirstName     string    `json:"first_name"`
-	LastName      string    `json:"last_name"`
-	Number        int64     `bun:",unique" json:"number"`
-	Balance       int64     `json:"balance"`
-	CreatedAt     time.Time `json:"createdAt"`
+	bun.BaseModel     `bun:"table:accounts,alias:a"`
+	ID                int       `bun:",pk,autoincrement" json:"id"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	Number            int64     `bun:",unique" json:"number"`
+	EncryptedPassword string    `json:"-"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"createdAt"`
 }
 
-func NewAccount(fn, ln string) *Account {
-	return &Account{
-		FirstName: fn,
-		LastName:  ln,
-		Number:    int64(rand.Intn(10000)),
-		CreatedAt: time.Now().UTC(),
+func NewAccount(fn, ln, password string) (*Account, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
 	}
+	return &Account{
+		FirstName:         fn,
+		LastName:          ln,
+		EncryptedPassword: string(encpw),
+		Number:            int64(rand.Intn(10000)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
